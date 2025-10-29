9f70757db6030804b35cd1a53a075269dde59aa5
Balaji Perumal <balajiperumaldev@gmail.com>
2025-10-29 17:09:17 +0530

    altered

--- practice_purpose/exercise/src/App.java
+++ practice_purpose/exercise/src/App.java
@@ -1,7 +1,22 @@
 package practice_purpose.exercise.src;
 
 public class App {
+
+    Integer value = null;
+	
+	App(){
+		
+	}
+	
+	App(int value){
+//		value = value;
+		this.value = value;
+	}
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("sample project");
+		App obj = new App();
+		System.out.println(obj.value);
+		obj = new App(22);
+		System.out.println(obj.value);
     }
 }
